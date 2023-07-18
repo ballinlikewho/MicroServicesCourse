e010ffb0aee7a8015ac056c9c62fe531fb9ba0be
Todd Feipel <todd.feipel@gmail.com>
2023-07-18 16:51:22 -0400

    initial commit

---
+++ PlatformService/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.EntityFrameworkCore;
+using PlatformService.AsyncDataServices;
+using PlatformService.Data;
+using PlatformService.SyncDataServices.Grpc;
+using PlatformService.SyncDataServices.Http;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+if (builder.Environment.IsProduction())
+{
+    Console.WriteLine("--> Using SqlServer Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMem Db");
+    builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMem"));
+}
+
+builder.Services.AddScoped<IPlatFormRepo, PlatformRepo>();
+builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
+builder.Services.AddSingleton<IMessageBusClient, MessageBusClient>();
+builder.Services.AddGrpc();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+}
+
+Console.WriteLine($"--> CommandService Endpoint {builder.Configuration["CommandService"]}");
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGrpcService<GrpcPlatformService>();
+
+PrepDb.PrepPopulation(app, app.Environment.IsProduction());
+
+app.Run();
